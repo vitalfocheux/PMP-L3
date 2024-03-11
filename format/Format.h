6b1a6237935a85f4c78afbb10a93011706116ba2
vitalfocheux <vitalfocheux@gmail.com>
2024-03-11 22:09:35 +0100

    Petite implem qui fonctionne sauf pour type générique

--- format/Format.h
+++ format/Format.h
@@ -1,18 +1,56 @@
 #ifndef FP_FORMAT_H
 #define FP_FORMAT_H
 
+#include <iostream>
+#include <sstream>
 #include <string>
+#include <tuple>
+
+// Prototype de format_tuple
+template<typename... Args, size_t... Is>
+void format_tuple(std::ostream& os, const std::string& fmt, const std::tuple<Args...>& args, std::index_sequence<Is...>);
+
+// Fonction de formatage récursive
+template<typename T>
+void format_arg(std::ostream& os, const std::string& fmt, const T& arg) {
+  std::cout << "format_arg recursive" << std::endl;
+  std::cout << arg << std::endl;
+  size_t pos = fmt.find("{}");
+  if (pos != std::string::npos) {
+    os << fmt.substr(0, pos);
+    os << arg;
+    format_arg(os, fmt.substr(pos + 2), arg);
+  } else {
+    os << fmt;
+  }
+}
+
+// Surcharge pour le cas de tuple
+template<typename... Args>
+void format_args(std::ostream& os, const std::string& fmt, const std::tuple<Args...>& args) {
+  std::cout << "format_args surcharge" << std::endl;
+  format_tuple(os, fmt, args, std::index_sequence_for<Args...>{});
+}
+
+// Fonction de formatage pour un tuple
+template<typename... Args, size_t... Is>
+void format_tuple(std::ostream& os, const std::string& fmt, const std::tuple<Args...>& args, std::index_sequence<Is...>) {
+  std::cout << "format_tuple" << std::endl;
+  (format_arg(os, fmt, std::get<Is>(args)), ...);
+}
 
 namespace fp {
 
   /**
    * @brief Insert all parameters into a formatted string
    */
-  std::string format(const std::string& formatString /* implementation defined */) {
-    return formatString;
+  template<typename... Args>
+  std::string format(const std::string& formatString, const Args&... args) {
+    std::cout << "format" << std::endl;
+    std::ostringstream oss;
+    format_args(oss, formatString, std::make_tuple(args...));
+    return oss.str();
   }
-
 }
-
 
 #endif // FP_FORMAT_H
--- format/testFormatPrint.cc
+++ format/testFormatPrint.cc
@@ -2,6 +2,69 @@
 
 #include "Format.h"
 
+
+namespace my {
+  struct Foo {
+    int i;
+  };
+
+  std::string to_string(const Foo& foo){
+    return std::to_string(foo.i);
+  }
+}
+
+TEST(FormetPrint, HelloWorld){
+  auto str = fp::format("%s %s!", "Hello", "World");
+  EXPECT_EQ(str, "Hello World!");
+}
+
+TEST(FormatPrint, String){
+  auto str = fp::format("The cake is lie!");
+  EXPECT_EQ(str, "The cake is lie!");
+}
+
+TEST(FormatPrint, Int){
+  auto str = fp::format("The Answer is %d", 42);
+  EXPECT_EQ(str, "The Answer is 42");
+}
+
+TEST(FormatPrint, Float){
+  auto str = fp::format("Approx of %s is %f", "Pi", 3.141592);
+  EXPECT_EQ(str, "Approx of Pi is 3.141592");
+}
+
+TEST(FormatPrint, Hexadecimal){
+  auto str = fp::format("%d in hexadecimal %x", 42, 42);
+  EXPECT_EQ(str, "42 in hexadecimal 0x2a");
+}
+
+TEST(FormatPrint, Character){
+  auto str = fp::format("'%c' is the 1st alphabet letter", 'A');
+  EXPECT_EQ(str, "'A' is the 1st alphabet letter");
+}
+
+TEST(FormatPrint, Boolean){
+  auto str = fp::format("This sentence is %d", false);
+}
+
+// TEST(FormatPrint, CustomType){
+//   my::Foo foo = {8};
+//   auto str = fp::format("%o", foo);
+//   EXPECT_EQ(str, "User implementation. Here: 8");
+// }
+
+TEST(FormatPrint, CustomTypeAddress){
+  my::Foo foo = {8};
+  auto str = fp::format("%p", &foo);
+  EXPECT_EQ(str, "Runtime address prefix by 0x");
+}
+
+TEST(FormatPrint, NoSibsistution){
+  auto str = fp::format("No substitution: %%i");
+  EXPECT_EQ(str, "No substitution: %i");
+}
+
+
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
